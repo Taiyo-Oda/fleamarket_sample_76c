d5d5f35ea96fe571bace4bed3f35bd312c8d6b63
Sho Tsuboya <sho_825.sa216@docomo.ne.jp>
2020-08-16 20:30:38 +0900

    add-profile,brand-table

---
+++ app/models/profile.rb
@@ -0,0 +1,16 @@
+class Profile < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  belongs_to :user
+
+  validates :family_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday, presence: true
+  validates :birthmonth, presence: true
+  validates :birthyear, presence: true
+
+end
---
+++ db/schema.rb
@@ -0,0 +1,34 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_08_13_105854) do
+
+  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "family_name", null: false
+    t.string "family_name_kana", null: false
+    t.string "first_name", null: false
+    t.string "first_name_kana", null: false
+    t.integer "birthday", null: false
+    t.integer "birthmonth", null: false
+    t.integer "birthyear", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
